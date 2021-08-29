2d54cdda5af69dcf9bbe7aae2ff5a2be1a0bfa59
SimiAlex <simineaalexandru@gmail.com>
2021-08-29 10:50:25 +0300

    Completed StringMessageListenerEjb to log the received messages

--- applicationB/src/main/java/SimiAlex/com/github/applicationB/ejb/StringMessageListenerEjb.java
+++ applicationB/src/main/java/SimiAlex/com/github/applicationB/ejb/StringMessageListenerEjb.java
@@ -1,18 +1,27 @@
 package SimiAlex.com.github.applicationB.ejb;
 
+import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
+import java.util.logging.Logger;
 
-@MessageDriven
+@MessageDriven(activationConfig = {
+        @ActivationConfigProperty(propertyName = "destination", propertyValue = "queue/stringQueue"),
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+        @ActivationConfigProperty(propertyName = "acknowledgment", propertyValue = "Auto-acknowledgment")
+})
 public class StringMessageListenerEjb implements MessageListener {
+
+    private  final Logger LOGGER = Logger.getLogger(StringMessageListenerEjb.class.toString());
 
     @Override
     public void onMessage(Message message) {
         try {
             String text =((TextMessage)message).getText();
+            LOGGER.info(text);
         } catch (JMSException e) {
             e.printStackTrace();
         }
